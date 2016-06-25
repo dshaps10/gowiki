b08129f1baeb5af39a80e0ecb36af9827c0d956e
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-06-24 20:35:20 -0700

    Add edit and save routes

--- wiki.go
+++ wiki.go
@@ -33,5 +33,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
