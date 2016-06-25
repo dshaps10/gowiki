8b94aa9f668352c87a79fc83350f54d11b454c96
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-06-25 09:51:03 -0700

    Add first draft of editHandler function

--- wiki.go
+++ wiki.go
@@ -37,3 +37,17 @@ func main() {
 	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprint(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
